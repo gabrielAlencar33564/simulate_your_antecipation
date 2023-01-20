c28f06de2dbb9ad03ea4cb899a1dcf5d6afa6353
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-19 23:44:48 -0300

    fix: set the results of anticipation in context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,13 +1,39 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState } from 'react'
 import { IAntecipationContext, IAntecipationProvider } from './interfaces'
+import { api } from '../services/api'
+import { SubmitHandler } from 'react-hook-form'
+import { IInputKeys } from './interfaces'
 
 const AntecipationContext = createContext<IAntecipationContext>(
   {} as IAntecipationContext,
 )
 
 export const AntecipationProvider = ({ children }: IAntecipationProvider) => {
+  const [days, setDays] = useState([])
+  console.log(days)
+  const postAntecipation: SubmitHandler<IInputKeys> = async (data) => {
+    const amount = data.amount
+      .split('')
+      .filter((value) => {
+        if (value !== '.') return value
+      })
+      .join('')
+      .replace(',', '.')
+
+    try {
+      const response = await api.post('', { ...data, amount: +amount })
+      setDays(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <AntecipationContext.Provider value={{}}>
+    <AntecipationContext.Provider
+      value={{
+        postAntecipation,
+      }}
+    >
       {children}
     </AntecipationContext.Provider>
   )
